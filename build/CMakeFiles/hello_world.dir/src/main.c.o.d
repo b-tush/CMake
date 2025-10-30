321ce0d7c18da65d51a4349ca9e62fda33aa84f1
Tushar Bhanarkar <tush.projects05@gmail.com>
2025-10-30 12:18:54 +0530

    Add files via upload

---
+++ build/CMakeFiles/hello_world.dir/src/main.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/hello_world.dir/src/main.c.o: \
+ /home/ifx/Tushar/test_code/cmake_demo/src/main.c \
+ /usr/include/stdc-predef.h \
+ /home/ifx/Tushar/test_code/cmake_demo/include/my_lib.h
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "my_lib.h"
+
+int main()
+{
+    say_hello();
+    return 0;
+}
